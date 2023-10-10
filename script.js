3d329990c20d511cb6e1fc69b28bc79984eb9326
Wojciech Pieczonka <wojciechpieczonka@gmail.com>
2023-10-10 12:36:44 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const converter = document.querySelector('#converter');
+const result = document.querySelector('.result');
+const convertBtn = document.querySelector('.convert');
+const resetBtn = document.querySelector('.reset');
+const changeBtn = document.querySelector('.change');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+
+//T (F) = T(C) x 1.8 + 32
+//T(C) = T(F) -32 / 1.8
+
+const swap = () => {
+	if (one.textContent === '°C') {
+		one.textContent = '°F';
+		two.textContent = '°C';
+	} else {
+		one.textContent = '°C';
+		two.textContent = '°F';
+	}
+};
+
+changeBtn.addEventListener('click', swap);
+
+const farToCel = () => {
+	farenheit = converter.value * 1.8 + 32;
+
+	result.textContent = `${converter.value} °C to ${farenheit.toFixed(1)} °F`;
+	converter.value = '';
+};
+
+const celToFar = () => {
+	celsius = (converter.value - 32) / 1.8;
+	result.textContent = `${converter.value} °F to ${celsius.toFixed(1)} °C `;
+	converter.value = '';
+};
+
+const conversion = () => {
+	if (converter.value !== '') {
+		if (one.textContent === '°C') {
+			farToCel();
+		} else {
+			celToFar();
+		}
+	} else {
+		result.textContent = 'Musisz podać jakąś wartość!';
+	}
+};
+
+convertBtn.addEventListener('click', conversion);
